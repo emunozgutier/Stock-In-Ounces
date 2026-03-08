0445b150b8749b7b908dee7419da8d1901ab4f4e
emunozgutier <emunozgutier@gmail.com>
2026-03-07 18:13:55 -0800

    Update TimeFrame.py

--- scripts/TimeFrame.py
+++ scripts/TimeFrame.py
@@ -25,7 +25,7 @@ def get_timeframe_dates():
     
     now = datetime.now()
     timeframes = {
-        "50y": 365 * 50,
+        "Max": 365 * 50,
         "20y": 365 * 20,
         "10y": 365 * 10,
         "5y": 365 * 5,
